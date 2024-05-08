bee0ae00f5c06c20188d52166271ca2ede0dd5b5
vidao1233 <vidao1233@gmail.com>
2024-05-08 13:23:35 +0700

    paging by letter

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -19,7 +19,7 @@ const Stack = createNativeStackNavigator()
 
 function App(props) {
     return <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login' screenOptions={{
+        <Stack.Navigator initialRouteName='Profile' screenOptions={{
             headerShown: false
         }}>
             <Stack.Screen name={"Welcome"} component={Welcome}/>
